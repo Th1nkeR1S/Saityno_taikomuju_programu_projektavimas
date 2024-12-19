ed6781bb87a07e0c8043b0871528e9430541b9d5
Th1nkeR1S <161242488+Th1nkeR1S@users.noreply.github.com>
2024-12-19 18:05:28 +0200

    4

--- obj/Release/net8.0/KasisAPI.AssemblyInfo.cs
+++ obj/Release/net8.0/KasisAPI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("KasisAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+931dbe83a307b7ee40a8f0e03a1a256835db911a")]
 [assembly: System.Reflection.AssemblyProductAttribute("KasisAPI")]
